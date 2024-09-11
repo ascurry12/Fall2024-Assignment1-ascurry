c9873a39961ab0f6ca6ec1d04104482ba561d2cc
Aniyah C. <aniyahcurry12@gmail.com>
2024-09-11 12:20:07 -0500

    uh

--- CS330-Assignment1-ascurry/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CS330_Assignment1_ascurry.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
