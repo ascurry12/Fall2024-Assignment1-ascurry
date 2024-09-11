d241b1cfa9f013723444556710caa18c1b14ba9e
Aniyah C. <aniyahcurry12@gmail.com>
2024-09-11 12:16:54 -0500

    rename

---
+++ Fall2024-Assignment1-ascurry/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CS330_Assignment1_ascurry.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
